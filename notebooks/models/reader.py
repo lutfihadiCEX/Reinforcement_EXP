d7f660c58483b8242104576eb38eb1f23654ceea
lutfihadiCEX <lutfihadij@gmail.com>
2025-12-09 22:02:06 +0800

    Temp files

--- notebooks/models/reader.py
+++ notebooks/models/reader.py
@@ -1,10 +1,9 @@
 import gymnasium as gym
 from stable_baselines3 import PPO
 
-# Load environment and trained model
-env = gym.make("CartPole-v1", render_mode="human")  # 'human' enables visual window
-model = PPO.load(r"C:\MLCourse\RLHF_Exploration\notebooks\models\cartpole_ppo.zip")  # Load your saved agent
 
+env = gym.make("CartPole-v1", render_mode="human")  
+model = PPO.load(r"C:\MLCourse\RLHF_Exploration\notebooks\models\cartpole_ppo.zip") 
 obs = env.reset()[0]
 done = False
 
@@ -12,6 +11,7 @@ while not done:
     action, _ = model.predict(obs)
     obs, reward, terminated, truncated, info = env.step(action)
     done = terminated or truncated
-    env.render()  # Shows the window with CartPole animation
+    env.render() 
 
 env.close()
+
